ae589329c080e6d334975fc29956304a69ea6da0
Oscar_HF13 <57047153+sibork13@users.noreply.github.com>
2020-04-23 12:49:41 -0500

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -50,6 +50,14 @@ def ConvertirToNumpyArray(Lista):
     Lista = np.array(Lista, np.uint8) #Convertimos la lista en una lista numpu
     return Lista
 
+def Obtener_Info_IR(Resultado):
+    # Obtenemos coordenadas de cada objeto reconocido (solo la esquina superior izquierda y la esquina inferior derecha)
+    IS = (Resultado['topleft']['x'], Resultado['topleft']['y'])# Coordenadas de la esquina superior izquierda
+    DI = (Resultado['bottomright']['x'], Resultado['bottomright']['y'])# Coordenadas de la esquina inferior derecha
+    etiqueta = Resultado['label'] # Clase a la que pertenece el reconocimiento
+    certeza = Resultado['confidence'] # Porcentaje de seguridad de pertenencia a la clase
+    return IS,DI,etiqueta,certeza
+
 
 def DibujarContornos(imagen,contornos,color,Palabra):
     for c in contornos:
@@ -62,3 +70,28 @@ def DibujarContornos(imagen,contornos,color,Palabra):
         approx = cv2.approxPolyDP(c,epsilon,True)
         cv2.drawContours(imagen,approx,0,color,2)
         cv2.putText(imagen,str(Palabra),(x,y),1,2,(0,0,0),2) # Pone texto
+
+
+def Identificar_Compa_Ene(Imagen,IzquierdaSuperior,DerechaInferior,Colores):
+    Imagen_Auxiliar = Imagen[IzquierdaSuperior[1]:DerechaInferior[1],IzquierdaSuperior[0]:DerechaInferior[0]]
+    # Conversion de colores
+    hsv_image = cv2.cvtColor(Imagen_Auxiliar,cv2.COLOR_BGR2HSV)#Convertimos BGR  a HSV para deteccion de colores
+    # Buscar areas de la imagen con el color definido
+    mascaraR = cv2.inRange(hsv_image,Colores[0],Colores[1])#verifica cuales pixeles  estan dentro del rango, los que no esten los hace negros
+    mascaraB = cv2.inRange(hsv_image,Colores[2],Colores[3])#verifica cuales pixeles  estan dentro del rango, los que no esten los hace negros
+    # Encontrar pixeles de contornos de objetos
+    ContornoRojo = cv2.findContours(mascaraR,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)[0]
+    ContornoAzul = cv2.findContours(mascaraB,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)[0]
+    # Dibujar contornos
+    DibujarContornos(Imagen_Auxiliar,ContornoRojo,(255,255,255),"Equipo")
+    DibujarContornos(Imagen_Auxiliar,ContornoAzul,(255,255,255),"Enemigo")
+    # Sustituimos el area de la imagen original con la reconocida
+    Imagen[IzquierdaSuperior[1]:DerechaInferior[1],IzquierdaSuperior[0]:DerechaInferior[0]] = Imagen_Auxiliar
+    return Imagen
+
+
+def Dibujar_Info(Imagen,IzquierdaSuperior,DerechaInferior,color,etiqueta,certeza):
+    text = '{}: {:.0f}%'.format(etiqueta, certeza * 100) # Texto a insertar
+    Imagen = cv2.rectangle(Imagen, IzquierdaSuperior, DerechaInferior, color, 5) # Dibujar rectangulo
+    Imagen = cv2.putText(Imagen, text, IzquierdaSuperior, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2) # Poner texto
+    return Imagen
